69e70981b97f80da56d9a5d827a537aa7ba40745
nihivis777 <zjf520zq@163.com>
2025-05-01 22:41:54 +0800

    20250501  v6.2.6 源码编译及新增自身测试工程

---
+++ spring-nihivis777-learn-demo/src/main/java/com/nihivis/learn/demo/Main.java
@@ -0,0 +1,14 @@
+package com.nihivis.learn.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+	@Test
+	public void test1(){
+		System.out.println("Hello, World!11111");
+	}
+}
